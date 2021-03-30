df5797d3a9c5b7c883acccea52d9e2d9d82d856c
Talsomech <Talsomech@gmail.com>
2021-03-30 12:10:46 +0300

    final finish

--- Board.cpp
+++ Board.cpp
@@ -6,51 +6,49 @@
 #include "Board.hpp"
 #include "Direction.hpp"
 #include <algorithm>
+
 using namespace std;
 namespace ariel {
-    void ariel::Board::post(const unsigned int row,const unsigned int col,const ariel::Direction dir, string msg) {
-        int lng = msg.size();
-        this->minrow=minrow<row?minrow:row;//std::min(minrow,row);
-        this->mincol=mincol<col?mincol:col;
+    void ariel::Board::post(const unsigned int row, const unsigned int col, const ariel::Direction dir, string msg) {
+        unsigned int lng = msg.size();
+        this->minrow = minrow < row ? minrow : row;
+        this->mincol = mincol < col ? mincol : col;
         switch (dir) {
             case Direction::Horizontal:
-                for (int i = col; i < col + lng; i++) {
-                    this->map[make_pair(row,i)]=msg[i-col];
+                for (unsigned int i = col; i < col + lng; i++) {
+                    this->map[make_pair(row, i)] = msg[i - col];
                 }
-                this->maxcol=maxcol>lng+col?maxcol:lng+col;
+                this->maxcol = maxcol > lng + col ? maxcol : lng + col;
                 break;
             case Direction::Vertical:
-                this->maxrow=maxrow>lng+row?maxrow:lng+row;
-                for (int j = row; j < row + lng; j++) {
-                    this->map[make_pair(j,col)]=msg[j-row];
+                this->maxrow = maxrow > lng + row ? maxrow : lng + row;
+                for (unsigned int j = row; j < row + lng; j++) {
+                    this->map[make_pair(j, col)] = msg[j - row];
                 }
                 break;
         }
-        //this->map[make_pair(row+1,col+1)]='\n';
     }
-    string ariel::Board::read(const unsigned int row,const  unsigned int col,const ariel::Direction dir, unsigned int length) {
+
+    string ariel::Board::read(const unsigned int row, const unsigned int col, const ariel::Direction dir,
+                              unsigned int length) {
         string ans;
         switch (dir) {
             case Direction::Horizontal:
-                for (int i = col; i < col + length; i++) {
+                for (unsigned int i = col; i < col + length; i++) {
                     if (this->map.find(make_pair(row, i)) == this->map.end()) {
                         ans += '_';
-                    }
-                    else {
-                        ans += this->map[make_pair(row,i)];
+                    } else {
+                        ans += this->map[make_pair(row, i)];
                     }
                 }
                 break;
             case Direction::Vertical:
-                for (int j = row; j < row + length; j++) {
-                    if(this->map.find(make_pair(j,col))==this->map.end()){
-                        ans+='_';
+                for (unsigned int j = row; j < row + length; j++) {
+                    if (this->map.find(make_pair(j, col)) == this->map.end()) {
+                        ans += '_';
+                    } else {
+                        ans += this->map[make_pair(j, col)];
                     }
-
-                    else {
-                        ans += this->map[make_pair(j,col)];
-                    }
-                    //ans+='\n';
                 }
                 break;
         }
@@ -58,19 +56,15 @@ namespace ariel {
     }
 
     void ariel::Board::show() {
-        minrow=minrow==0?1:minrow;
-        mincol=mincol==0?2:mincol;
-        for (int i = minrow-1; i < maxrow+1; ++i) {
-            cout<<"row "<<i<<": ";
-            for (int j = mincol-2; j <maxcol+3; ++j) {
-                cout<<read(i,j,Direction::Horizontal,1);
+        minrow = minrow == 0 ? 1 : minrow;
+        mincol = mincol == 0 ? 2 : mincol;
+        for (unsigned int i = minrow - 1; i < maxrow + 1; ++i) {
+            cout << "row " << i << ": ";
+            for (unsigned int j = mincol - 2; j < maxcol + 3; ++j) {
+                cout << read(i, j, Direction::Horizontal, 1);
             }
-            cout<<endl;
+            cout << endl;
         }
-//        for(auto x : this->map){
-//            cout<<x.second;
-//            cout<<x.first.first<<","<<x.first.second;
-//        }
-        //cout<<"not implemented yet"<<endl;
+
     }
 }
--- Board.hpp
+++ Board.hpp
@@ -8,29 +8,29 @@
 #include "Direction.hpp"
 #include <unordered_map>
 #include <limits>
+
 using namespace std;
+
 struct hash_pair {
-    template <class T1, class T2>
-    size_t operator()(const pair<T1, T2>& p) const{
-        auto hash1 = hash<T1>{}(p.first);
-        auto hash2 = hash<T2>{}(p.second);
+    template<class T1, class T2>
+    size_t operator()(const pair <T1, T2> &p) const {
+        auto hash1 = hash < T1 > {}(p.first);
+        auto hash2 = hash < T2 > {}(p.second);
         return hash1 ^ hash2;
     }
 };
 namespace ariel {
     class Board {
     private:
-        int mincol=std::numeric_limits<int>::max();
-        int maxcol=0;
-        int maxrow=0;
-        int minrow=std::numeric_limits<int>::max();
-        //int maxlng=0;
-        //unordered_map<int,unordered_map<int,char>> map;
-        unordered_map<pair<int,int>,char,hash_pair> map;
-        //unordered_map<pair<int, int>, char, boost::hash>> map;
+        unsigned int mincol = std::numeric_limits<int>::max();
+        unsigned int maxcol = 0;
+        unsigned int maxrow = 0;
+        unsigned int minrow = std::numeric_limits<int>::max();
+        unordered_map<pair<int, int>, char, hash_pair> map;
     public:
-        void post(unsigned int row,unsigned int col,const ariel::Direction dir, string msg);
-        string read(unsigned int row,unsigned int col, ariel::Direction dir,unsigned int length);
+        void post(unsigned int row, unsigned int col, const ariel::Direction dir, string msg);
+
+        string read(unsigned int row, unsigned int col, ariel::Direction dir, unsigned int length);
         //string read(const unsigned int row, int col,const ariel::Direction dir,const unsigned int length);
 
         void show();
